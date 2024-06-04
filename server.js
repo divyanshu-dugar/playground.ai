b8b667c2b1ae1c31383754705350574d665cf805
Divyanshu Dugar <divyu2004@gmail.com>
2024-06-04 17:20:02 -0400

    added data and modules directory

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+// This line imports the Express module, which is a web application framework for Node.js designed for building web applications and APIs.
 const express = require("express")
 
+// This line creates an instance of an Express application. This app object is used to set up middleware and define routes for handling HTTP requests.
 const app = express()
+// This line creates an instance of an Express application. This app object is used to set up middleware and define routes for handling HTTP requests.
 const PORT = 8080
 
 app.get("/", (req, res) => {
-    res.send("hello")
+    res.send("hello world")
 })
 
 app.listen(PORT, () => {
