6ba70561549d7150fc47ab6aca22098bfa35e2b6
Divyanshu Dugar <divyu2004@gmail.com>
2024-06-12 14:38:07 -0400

    Added views and public css

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 // This line imports the Express module, which is a web application framework for Node.js designed for building web applications and APIs.
 const express = require("express")
-
+//
 const modelService = require("./modules/modelServices");
+//
+const path = require("path");
 
 // This line creates an instance of an Express application. This app object is used to set up middleware and define routes for handling HTTP requests.
 const app = express()
 // This line creates an instance of an Express application. This app object is used to set up middleware and define routes for handling HTTP requests.
 const PORT = 8080
+//
+
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-    res.send("hello world")
+    res.sendFile(path.join(__dirname, "/views/index.html"))
 })
 
 app.get("/models", (req, res) => {
